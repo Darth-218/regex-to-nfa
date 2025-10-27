5d6df4b74e866f0147b5c4ba281c380c018a593b
Yahia Gaber <yahia.hany.gaber@proton.me>
2025-10-27 13:19:44 +0300

    feat: Created base UI and a CLI script

---
+++ cli.py
@@ -0,0 +1,10 @@
+import parser, visualizer, constructor
+
+while True:
+    regex = input("Enter regex to parse: ")
+
+    ast = parser.Parser().parse(regex)
+    nfa = constructor.construct_nfa(ast)
+
+    visualizer.GraphVisualizer().visualize_nfa(nfa)
+    visualizer.GraphVisualizer().visualize_ast(ast)
--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-import parser, visualizer, constructor
+import parser, constructor, visualizer
+import streamlit as st
 
-regex = input("Enter regex to parse: ")
+st.header("Regex to NFA Converter")
 
-ast = parser.Parser().parse(regex)
-nfa = constructor.construct_nfa(ast)
+regex = st.text_input("Enter Regex")
+clicked = st.button("Convert")
 
-visualizer.GraphVisualizer().visualize_nfa(nfa)
-visualizer.GraphVisualizer().visualize_ast(ast)
+def process():
+    try:
+        ast = parser.Parser().parse(regex)
+    except SyntaxError as e:
+        st.error(e)
+        return
+
+    nfa = constructor.construct_nfa(ast)
+
+    nfa_visualized = visualizer.GraphVisualizer().visualize_nfa(nfa)
+    ast_visualized = visualizer.GraphVisualizer().visualize_ast(ast)
+
+    st.subheader(f"For the Regex `{regex}`:")
+
+    nfa_tab, ast_tab, all_tab = st.tabs(["NFA", "AST", "All"])
+
+    with nfa_tab:
+        st.image(nfa_visualized, use_container_width=True)
+
+    with ast_tab:
+        _, col, _ = st.columns([1, 2, 1])
+        with col:
+            st.image(ast_visualized, use_container_width=True)
+
+    with all_tab:
+
+        st.markdown(f"### The NFA")
+        st.image(nfa_visualized, use_container_width=True)
+
+        st.markdown(f"#### The AST")
+        _, col, _ = st.columns([1, 2, 1])
+        with col:
+            st.image(ast_visualized, use_container_width=True)
+
+if (clicked and regex) or regex:
+    process()
